963104af2d53f2c9fb5aef27fe2587bc0cf89e1d
RJ <ballerarge@gmail.com>
2018-06-03 12:05:11 -0500

    First commit

---
+++ Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	[Header ("Set in Inspector")]
+	public GameObject player;
+	public float cameraDistance;
+
+	void Start() {
+	}
+
+	void LateUpdate() {
+		transform.position = player.transform.position - player.transform.forward * cameraDistance;
+		transform.LookAt (player.transform.position);
+	}
+}
---
+++ Assets/_Scripts/Walking.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walking : MonoBehaviour {
+
+	[Header ("Set in Inspector")]
+	public float rotationSpeed;
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animator> ();
+		anim.SetTrigger ("Idle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.W)) {
+			anim.SetTrigger ("Running");
+		} else if (Input.GetKeyUp (KeyCode.W)) {
+			anim.SetTrigger ("Idle");
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			anim.SetTrigger ("RunningBackwards");
+		} else if (Input.GetKeyUp (KeyCode.S)) {
+			anim.SetTrigger ("Idle");
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetTrigger ("Jump");
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (0, -rotationSpeed, 0);
+			Camera.main.transform.Rotate (0, -rotationSpeed, 0);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate (0, rotationSpeed, 0);
+			Camera.main.transform.Rotate (0, rotationSpeed, 0);
+		}
+	}
+}
