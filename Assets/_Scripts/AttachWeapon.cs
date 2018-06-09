cf27019f12bd6a206e3bbf12628dada61810cdc5
RJ <ballerarge@gmail.com>
2018-06-09 14:21:55 -0500

    Found finger in script, will attach weapon dynamically later

---
+++ Assets/_Scripts/AttachWeapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachWeapon : MonoBehaviour {
+	public GameObject weapon;
+
+	// Use this for initialization
+	void Start () {
+		
+		Transform rightMFinger = FindDeepChild (transform, "MiddleFinger1_R");
+		print (rightMFinger.name);
+	}
+
+	public Transform FindDeepChild(Transform aParent, string aName) {
+		var result = aParent.Find (aName);
+		if (result != null) {
+			return result;
+		}
+
+		foreach (Transform child in aParent) {
+			result = FindDeepChild (child, aName);
+			if (result != null) {
+				return result;
+			}
+		}
+		return null;
+	}
+}
