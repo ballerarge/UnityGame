e1790c5143a64e4a656e105ddf468279aba8d532
RJ <ballerarge@gmail.com>
2018-06-11 17:36:05 -0500

    Chain falling, moving like supposed to. Next up is making it rotate and follow character correctly

--- Assets/_Scripts/AttachWeapon.cs
+++ Assets/_Scripts/AttachWeapon.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class AttachWeapon : MonoBehaviour {
 	public GameObject weapon;
+	private Transform finger;
 
 	// Use this for initialization
 	void Start () {
-		
-		Transform rightMFinger = FindDeepChild (transform, "MiddleFinger1_R");
-		print (rightMFinger.name);
+		finger = FindDeepChild (transform, "MiddleFinger1_R");
+	}
+
+	void LateUpdate() {
+		weapon.transform.position = finger.position;
+//		weapon.transform.rotation = finger.rotation;
 	}
 
 	public Transform FindDeepChild(Transform aParent, string aName) {
---
+++ Assets/_Scripts/AutoConnectJoint.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoConnectJoint : MonoBehaviour {
+
+	void Awake() {
+		GetComponent<CharacterJoint> ().connectedBody = transform.parent.GetComponent<Rigidbody> ();
+	}
+}
