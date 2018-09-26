baad10a497854c3b42a51798f3fac3e32c542dac
RJ DeCramer <ballerarge@gmail.com>
2018-09-25 21:35:50 -0500

    Got a functional jump working. It satisfies me for the time being, but it's nothing too special.

--- Assets/_Scripts/Walking.cs
+++ Assets/_Scripts/Walking.cs
@@ -9,13 +9,23 @@ public class Walking : MonoBehaviour {
     public Transform characterFollow;
     public float maxRot;
     public float rotFollowSpeed;
-
+    public Vector3 jump;
+    public float jumpForce;
+    public float jumpTimer = 0.0f;
+    public float maxJumpSeconds;
+    public float fallTimer = 0.0f;
+    public float maxFallSeconds;
+    
+    public bool jumping;
+    public bool falling;
+    Rigidbody rb;
 	Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		anim = gameObject.GetComponent<Animator> ();
 		anim.SetTrigger ("Idle");
+		rb = transform.parent.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -25,6 +35,8 @@ public class Walking : MonoBehaviour {
             checkRotation();
         }
 
+        // Animator code that fires events for
+        // character running.
         if (Input.GetKeyDown (KeyCode.W)) {
 			anim.SetTrigger ("Running");
             checkRotation();
@@ -39,11 +51,42 @@ public class Walking : MonoBehaviour {
 			anim.SetTrigger ("Idle");
         }
 
+        // Jumping code. If character is not in process of jumping or falling,
+        // space can be pressed to initiate jump.
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			anim.SetBool ("JumpFinished", false);
-			anim.SetTrigger ("Jump");
+			if (!jumping && !falling) {
+				jumping = true;
+				rb.useGravity = false;
+				rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+			}
         }
 
+        // If jumping, timer counts up to max time
+        // before character should begin to fall.
+        if (jumping) {
+        	jumpTimer += Time.deltaTime;
+        	float seconds = jumpTimer % 60;
+        	if (seconds >= maxJumpSeconds) {
+        		rb.useGravity = true;
+        		jumping = false;
+        		falling = true;
+        		jumpTimer = 0;
+        	}
+        }
+
+        // If falling, timer counts up to max time
+        // before character is allowed to jump again.
+        if (falling) {
+        	fallTimer += Time.deltaTime;
+        	float seconds = fallTimer % 60;
+			if (seconds >= maxFallSeconds) {
+        		falling = false;
+        		fallTimer = 0;
+        	}
+        }
+
+        // Rotation code for movement. Rotates character
+        // and the object that the camera follows.
 		if (Input.GetKey (KeyCode.A)) {
 			transform.Rotate (0, -rotationSpeed, 0);
             characterFollow.Rotate(0, -rotationSpeed, 0);
@@ -53,19 +96,6 @@ public class Walking : MonoBehaviour {
 			transform.Rotate (0, rotationSpeed, 0);
             characterFollow.Rotate(0, rotationSpeed, 0);
         }
-
-		if (anim.GetBool ("JumpFinished")) {
-			if (Input.GetKey (KeyCode.W)) {
-                anim.SetTrigger ("Running");
-                checkRotation();
-            } else if (Input.GetKey (KeyCode.S)) {
-                anim.SetTrigger ("RunningBackwards");
-                checkRotation();
-            } else {
-				anim.SetTrigger ("Idle");
-			}
-			anim.SetBool ("JumpFinished", false);
-		}
 	}
 
     void checkRotation()
@@ -73,7 +103,6 @@ public class Walking : MonoBehaviour {
         if ((Mathf.Abs(characterFollow.localEulerAngles.y) - Mathf.Abs(transform.localEulerAngles.y) > maxRot)
             || (Mathf.Abs(characterFollow.localEulerAngles.y) - Mathf.Abs(transform.localEulerAngles.y) < -maxRot))
         {
-            // characterFollow.rotation = Quaternion.Lerp(characterFollow.rotation, transform.rotation, 0.5f);
             characterFollow.rotation = Quaternion.RotateTowards(characterFollow.rotation, transform.rotation, rotFollowSpeed * Time.deltaTime);
 
         }
