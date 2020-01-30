d03c5eb830725105097149f3ef68f5e3e6725778
RJ DeCramer <ballerarge@gmail.com>
2020-01-29 22:20:32 -0600

    Worked on movement for no-art characters. Close to getting camera rotation working correctly again

--- Assets/_Scripts/Characters/Akko.cs
+++ Assets/_Scripts/Characters/Akko.cs
@@ -6,7 +6,9 @@ public class Akko : MonoBehaviour, ICharacter
 {
 
     private string charName;
-    private int health;
+    public int health;
+    public int move_speed;
+    readonly int starting_health = 100;
     public GameObject physAttack;
     public GameObject specialAttack1;
     public GameObject specialAttack2;
@@ -16,25 +18,43 @@ public class Akko : MonoBehaviour, ICharacter
     // Start is called before the first frame update
     void Start()
     {
-        
+        health=starting_health;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W)) {
+			moveForwards();
+        }
+
+		if (Input.GetKey(KeyCode.S)) {
+			moveBackwards();
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            moveLeft();
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            moveRight();
+        }
     }
 
     public void moveForwards() {
+        this.transform.Translate(Vector3.forward * Time.deltaTime, Space.World);
     }
 
     public void moveBackwards() {
+        this.transform.Translate(Vector3.back * Time.deltaTime, Space.World);
     }
 
     public void moveLeft() {
+        this.transform.Translate(Vector3.left * Time.deltaTime, Space.World);
     }
 
     public void moveRight() {
+        this.transform.Translate(Vector3.right * Time.deltaTime, Space.World);
     }
 
     public void jump() {
---
+++ Assets/_Scripts/Other/CameraRotate.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public Transform objectToRotateAround;
+    public int camRotSpeed;
+    private bool isCamMoving;
+
+    private Directions directions;
+
+    void Start() {
+        isCamMoving=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.mousePosition.x > Screen.width - 30 && (!isCamMoving ^ (directions==Directions.up))) {
+            //up
+            transform.RotateAround(objectToRotateAround.position, Vector3.up, camRotSpeed * Time.deltaTime);
+            isCamMoving=true;
+            directions = Directions.up;
+        }
+        else if (Input.mousePosition.x < 30 && (!isCamMoving ^ (directions==Directions.down))) {
+            //down
+            transform.RotateAround(objectToRotateAround.position, Vector3.down, camRotSpeed * Time.deltaTime);
+            isCamMoving=true;
+            directions = Directions.down;
+        }
+        else if (Input.mousePosition.y > Screen.height - 30 && (!isCamMoving ^ (directions==Directions.left))) {
+            //left
+            transform.RotateAround(objectToRotateAround.position, Vector3.left, camRotSpeed * Time.deltaTime);
+            isCamMoving=true;
+            directions = Directions.left;
+        }
+        else if (Input.mousePosition.y < 30 && (!isCamMoving ^ (directions==Directions.right))) {
+            //right
+            transform.RotateAround(objectToRotateAround.position, Vector3.right, camRotSpeed * Time.deltaTime);
+            isCamMoving=true;
+            directions = Directions.right;
+        }
+        else {
+            isCamMoving=false;
+        }
+    }
+
+    enum Directions {
+        up,
+        down,
+        left,
+        right
+    }
+}
