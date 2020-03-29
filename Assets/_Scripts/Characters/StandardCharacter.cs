8b105d60df0de9e730d57c669f4540e9a584de15
RJ DeCramer <ballerarge@gmail.com>
2020-03-29 15:00:29 -0500

    Movement script in a stable state

--- Assets/_Scripts/Characters/StandardCharacter.cs
+++ Assets/_Scripts/Characters/StandardCharacter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StandardCharacter : MonoBehaviour, ICharacter
+public class StandardCharacter : MonoBehaviour
 {
     Animator mAnim;
     private string charName;
@@ -14,71 +14,29 @@ public class StandardCharacter : MonoBehaviour, ICharacter
     public GameObject specialAttack2;
     public GameObject ultAttack;
 
+    private MovementDirection directionState;
 
     // Start is called before the first frame update
     void Start()
     {
         health=starting_health;
         mAnim = GameObject.FindWithTag("Player").GetComponent<Animator>();
-        
+        directionState = new MovementDirection(mAnim);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) {
-			moveForwards();
-        }
-
-        if(Input.GetKeyUp(KeyCode.W)) {
-            mAnim.SetTrigger("Idle");
-        }
-        
-        if (Input.GetKey(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) {
             faceCamera();
         }
 
-		if (Input.GetKey(KeyCode.S)) {
-			moveBackwards();
-        }
+        directionState.checkDirection(mAnim);
 
-        if (Input.GetKey(KeyCode.A)) {
-            moveLeft();
-        }
-
-        if (Input.GetKey(KeyCode.D)) {
-            moveRight();
-        }
-
-        if (Input.GetKey(KeyCode.Space)) {
-            jump();
-        }
     }
 
     public void faceCamera() {
         transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
-    }
-
-    public void moveForwards() {
-        //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
-        //transform.Translate(transform.forward * Time.deltaTime * move_speed, Space.World);
-        mAnim.SetTrigger("Running");
-        
-    }
-
-    public void moveBackwards() {
-        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
-        transform.Translate(-1 * transform.forward * Time.deltaTime * move_speed, Space.World);
-    }
-
-    public void moveLeft() {
-        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
-        transform.Translate(-1 * transform.right * Time.deltaTime * move_speed, Space.World);
-    }
-
-    public void moveRight() {
-        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, transform.localEulerAngles.z);
-        transform.Translate(transform.right * Time.deltaTime * move_speed, Space.World);
     }
 
     public void jump() {
@@ -89,4 +47,123 @@ public class StandardCharacter : MonoBehaviour, ICharacter
 
     public void performAttack() {
     }
+}
+
+public class MovementDirection {
+    private Direction currentDirection;
+    private bool forward;
+    private bool backward;
+    private bool left;
+    private bool right;
+
+    public MovementDirection(Animator mAnim) {
+        forward = false;
+        backward = false;
+        left = false;
+        right = false;
+        currentDirection = Direction.none;
+        mAnim.SetTrigger("Idle");
+    }
+
+    public void checkDirection(Animator mAnim) {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            moveForward(mAnim);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W)) {
+            stopMovingForward(mAnim);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            moveLeft(mAnim);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A)) {
+            stopMovingLeft(mAnim);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+            moveBackwards(mAnim);
+        }
+
+        if (Input.GetKeyUp(KeyCode.S)) {
+            stopMovingBackwards(mAnim);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)) {
+            moveRight(mAnim);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D)) {
+            stopMovingRight(mAnim);
+        }
+    }
+
+    public void moveForward(Animator mAnim) {
+        forward = true;
+        currentDirection = Direction.forward;
+        mAnim.SetTrigger("Running");
+    }
+    public void stopMovingForward(Animator mAnim) {
+        forward = false;
+        determineCurrentDirection(mAnim);
+    }
+    public void moveBackwards(Animator mAnim) {
+        backward = true;
+        currentDirection = Direction.backward;
+        mAnim.SetTrigger("RunningBackwards");
+    }
+    public void stopMovingBackwards(Animator mAnim) {
+        backward = false;
+        determineCurrentDirection(mAnim);
+    }
+    public void moveLeft(Animator mAnim) {
+        left = true;
+        currentDirection = Direction.left;
+        mAnim.SetTrigger("RunLeft");
+    }
+    public void stopMovingLeft(Animator mAnim) {
+        left = false;
+        determineCurrentDirection(mAnim);
+    }
+    public void moveRight(Animator mAnim) {
+        right = true;
+        currentDirection = Direction.right;
+        mAnim.SetTrigger("RunRight");
+    }
+    public void stopMovingRight(Animator mAnim) {
+        right = false;
+        determineCurrentDirection(mAnim);
+    }
+
+    public Direction getCurrentDirection() {
+        return currentDirection;
+    }
+
+    private void determineCurrentDirection(Animator mAnim) {
+        if (forward) {
+            currentDirection = Direction.forward;
+            mAnim.SetTrigger("Running");
+        } else if (backward) {
+            currentDirection = Direction.backward;
+            mAnim.SetTrigger("RunningBackwards");
+        } else if (left) {
+            currentDirection = Direction.left;
+            mAnim.SetTrigger("RunLeft");
+        } else if (right) {
+            currentDirection = Direction.right;
+            mAnim.SetTrigger("RunRight");
+        } else {
+            currentDirection = Direction.none;
+            mAnim.SetTrigger("Idle");
+        }
+    }
+}
+
+public enum Direction {
+    forward,
+    backward,
+    left,
+    right,
+    none
 }
